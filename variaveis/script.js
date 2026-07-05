e9a301a12268310035ac21a3e4d153d3adaea128
Malaki <jeffersondasilva.malaki@gmail.com>
2026-07-05 20:58:35 -0300

    sintaxe das variaveis

---
+++ variaveis/script.js
@@ -0,0 +1,11 @@
+var nome = "Jefferson" // Praticamente não utilizamos mais essa declaração
+let sobreNome = "Malaki" // Let é muito usada, é uma variavel que pode ser atualizada
+const apelido = "Malak" // Const é uma variavel constante, ou seja ela não atualiza
+
+console.log(nome,sobreNome,apelido)
+
+let preco = 5
+let totalComprado = 2
+let totalPreco = preco * totalComprado // Utilizei de duas variaveis para multiplicar entre elas, então perco a necessidade de ficar reescrevendo linha de codigo repetidas
+
+console.log(totalPreco)
