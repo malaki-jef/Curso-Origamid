9f0ff62ce766329db85d4f0812f42a8635cc47ca
Malaki <jeffersondasilva.malaki@gmail.com>
2026-07-06 00:20:00 -0300

    Numeros e Operadores

---
+++ numeros-operadores/script.js
@@ -0,0 +1,14 @@
+// NUMEROS
+let idade = 30
+let gols = 1000
+let pi = 3.14
+let exp = 2e10  
+console.log(idade,gols,pi,exp)
+
+// OPERADORES ARITMETICOS
+let soma = 100 + 50; // 150
+let subtracao = 100 - 50; // 50
+let multiplicacao = 100 * 2; // 200
+let divisao = 100 / 2; // 50
+let expoente = 2 ** 4; // 16
+let modulo = 14 % 5; // 4
