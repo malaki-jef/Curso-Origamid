cc839fd665165c528cee7eb14116e2b090cd7805
Malaki <jeffersondasilva.malaki@gmail.com>
2026-07-05 21:27:19 -0300

    Os 7 tipos de dados

---
+++ tipo-de-dados/script.js
@@ -0,0 +1,7 @@
+let nome = "Jefferson"; // String
+let idade = 30; // Number
+let possuiFaculdade = true; // Bolean (boleano)
+let time; // Undefined
+let comida = null; // Null
+let simbolo = Symbol() //Symbol
+let novoObjeto = {} // Object
