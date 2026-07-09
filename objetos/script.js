87996f6114b66983af469266955d1825da5bad49
Malaki <jeffersondasilva.malaki@gmail.com>
2026-07-09 15:10:17 -0300

    Exercicios

--- objetos/script.js
+++ objetos/script.js
@@ -72,3 +72,31 @@ let menuObj = {
     }
 }
 console.log(menuObj.metadeTamanho()); // Para passar o argumento de uma função que esta dentro do objeto é preciso chamar o objeto e dar . e nome da função, ai sim passar o argumento!
+
+
+// EXERCICIO
+// Crie um objeto com os seus dados pessoais Deve possui pelo menos duas propriedades nome e sobrenome
+let dadosPessoais = {
+    nome: 'Jefferson',
+    sobreNome: 'Malaki',
+    idade: 30,
+    hobbie: 'Futebol',
+    nomeCompleto() {
+        return `${this.nome} ${this.sobreNome}`
+    }
+}
+// Crie um método no objeto anterior, que mostre o seu nome completo
+console.log(dadosPessoais.nomeCompleto())
+
+
+// Modifique o valor da propriedade preco para 3000
+var carro = {
+    preco: 1000,
+    portas: 4,
+    marca: 'Audi',
+  }
+  
+  // Crie um objeto de um cachorro que represente um labrador,
+  // preto com 10 anos, que late ao ver um homem
+  
+  
