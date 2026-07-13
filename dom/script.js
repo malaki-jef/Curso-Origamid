aa8ba384c8c3be606dba69995261728c977714fd
Malaki <jeffersondasilva.malaki@gmail.com>
2026-07-12 23:48:43 -0300

    DOM

---
+++ dom/script.js
@@ -0,0 +1,13 @@
+// Retorne o url da página atual utilizando o objeto window
+const urlSite = window.location.href
+console.log(urlSite)
+
+// Seleciona o primeiro elemento da página que possua a classe ativo
+const titulo = document.querySelector('.ativo')
+
+// Retorne a linguagem do navegador
+console.log(navigator.language
+
+)
+// Retorne a largura da janela
+console.log(window.innerWidth,window.innerHeight); 
