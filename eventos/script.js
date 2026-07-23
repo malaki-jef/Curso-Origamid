1428deced9a69c7bf9d977128844d6eddf8e9d3e
Malaki <jeffersondasilva.malaki@gmail.com>
2026-07-23 10:01:04 -0300

    Atualização dos exercicios

--- eventos/script.js
+++ eventos/script.js
@@ -3,6 +3,7 @@
 /*
 É boa prática separar a função de callback do addEventListener, ou seja, declarar uma função ao invés de passar diretamente uma função anônima
 */
+/*
 const img = document.querySelector('img');
 function callback() {
   console.log('Clicou');
@@ -42,10 +43,11 @@ currentTarget =	Elemento com o listener
 target =	Elemento clicado de fato
 type =	Tipo do evento ("click", etc)
 composedPath() =	Caminho de propagação (substitui path)
-*/
+
 
 
 // FOREACH E EVENTOS
+
 const imagens = document.querySelectorAll('img');
 
 function imgSrc(event) {
@@ -56,3 +58,41 @@ function imgSrc(event) {
 imagens.forEach((img) => {
   img.addEventListener('click', imgSrc);
 });
+ */
+
+// EXERCICIOS
+
+// Quando o usuário clicar nos links internos do site, adicione a classe ativo ao item clicado e remova dos demais itens caso eles possuam a mesma. Previna o comportamento padrão desses links
+const todosLinks = document.querySelectorAll('a[href^="#"]');
+
+function classAtivo(event){
+  event.preventDefault();
+  todosLinks.forEach(function(link){
+    link.classList.remove('ativo')
+  });
+  event.currentTarget.classList.add('ativo');
+};
+
+todosLinks.forEach(function(event) {
+  event.addEventListener('click', classAtivo);
+});
+
+// Selecione todos os elementos do site começando a partir do body, ao clique mostre exatamente quais elementos estão sendo clicados
+const body = document.querySelectorAll('body');
+body.forEach(function(el){
+  el.addEventListener('click', function(tar){
+    console.log(tar.target);
+  })
+
+});
+
+// Utilizando o código anterior, ao invés de mostrar no console, remova o elemento que está sendo clicado, o método remove() remove um elemento
+const bodyRemove = document.querySelectorAll('body');
+bodyRemove.forEach(function(el){
+  el.addEventListener('click', function(tar){
+    const target = tar.target;
+    target.remove(target)
+  });
+});
+
+// Se o usuário clicar na tecla (t), aumente todo o texto do site. 
