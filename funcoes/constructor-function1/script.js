c80630af6edddd652b8908d0ac8cfa6e521b563a
Malaki <jeffersondasilva.malaki@gmail.com>
2026-08-18 09:35:57 -0300

    exercicios realizados

--- funcoes/constructor-function1/script.js
+++ funcoes/constructor-function1/script.js
@@ -38,5 +38,57 @@ function Carro(marca, precoInicial) {
     console.log(this);
   }
   
-  const honda = new Carro('Honda', 2000);
+const honda = new Carro('Honda', 2000);
+
+
+// EXERCICIOS
+
+// Transforme o objeto abaixo em uma Constructor Function
+const pessoa = {
+    nome: 'Nome pessoa',
+    idade: 0,
+    andar() {
+      console.log(this.nome + ' andou');
+    }
+}
+
+function Pessoa (nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
+    this.andar = function () {
+        console.log(`${this.nome} andou!!`);
+    };
+}
+const malaki = new Pessoa('Malaki', 30)
+console.log(malaki)
+console.log(malaki.andar())
+
+// Crie 3 pessoas, João - 20 anos,
+// Maria - 25 anos, Bruno - 15 anos
   
+const joao = new Pessoa ('João', 20);
+const maria = new Pessoa ('Maria', 25);
+const bruno = new Pessoa ('Bruno', 15);
+
+
+  
+// Crie uma Constructor Function (Dom) para manipulação de listas de elementos do dom. Deve conter as seguintes propriedades e métodos: Elements: retorna NodeList com os elementos selecionados -- addClass(classe): adiciona a classe a todos os elementos -- removeClass(classe): remove a classe a todos os elementos
+  
+function Dom (seletor) {
+   const elementList = document.querySelectorAll(seletor);
+   this.elements = elementList;
+   this.addClass = function(classe){     
+        elementList.forEach((element) => {
+        element.classList.add(classe);
+    })
+    }
+    this.remove = function(classe){     
+        elementList.forEach((element) => {
+        element.classList.remove(classe);
+    })
+   }
+}
+
+const listaItens = new Dom ('li')
+listaItens.addClass('Ativo');
+//listaItens.remove('Ativo')
