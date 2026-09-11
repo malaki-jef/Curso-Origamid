f10c75503713cc7528175c6bd4f189392a960167
Malaki <jeffersondasilva.malaki@gmail.com>
2026-09-11 14:52:21 -0300

    Iniciando estudo de React

---
+++ reactCompleto/src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+
+const App = () => {
+    return <a href="https://google.com.br">Google</a>
+}
+
+export default App;
---
+++ reactCompleto/src/main.jsx
@@ -0,0 +1,8 @@
+import ReactDom from 'react-dom/client';
+import React from 'react';
+
+import App from './App.jsx'
+
+
+
+ReactDom.createRoot(document.getElementById('root')).render(<App />);
